4373b2027d4bb7ea012d5266ccf7415532a46d9b
unknown <tungtt@vietmoz.edu.vn>
2018-09-24 15:09:09 +0700

    ver 1.2.1

--- pure-kenhsango.php
+++ pure-kenhsango.php
@@ -5,7 +5,7 @@
  * Author: Vietmoz
  * Author URI: http://thietkeweb.vietmoz.com/vietmoz
  * Description: Plugin Pure KenhSanGo custom for kenhsango.com
- * Version: 1.2.0
+ * Version: 1.2.1
  * Text Domain: pure
  */
 
